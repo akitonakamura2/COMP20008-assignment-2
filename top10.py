be69d3761070affc2b16034e81cf1b04eec445a3
kaihuazhu <kaihua@student.unimelb.edu.au>
2021-05-20 16:02:55 +1000

    fixed top10

--- top10.py
+++ top10.py
@@ -7,19 +7,22 @@ c1 = preprocess_crime1("crime1.csv")
 one_bf = preprocess_combined(h1, c1)
 
 livabilities = {}
-for i in range(620):
-    one_bf["Livability"] = 1/(one_bf["Median House Price"] * one_bf["Rate per 100,000 population"])
+counts = {}
+one_bf["Livability"] = 1/(one_bf["Median House Price"] * one_bf["Rate per 100,000 population"])
+for i in range(len(one_bf)):
     if one_bf["LGA"][i] in livabilities:
         livabilities[one_bf["LGA"][i]] += one_bf["Livability"][i]
+        counts[one_bf["LGA"][i]] += 1
     else:
         livabilities[one_bf["LGA"][i]] = one_bf["Livability"][i]
+        counts[one_bf["LGA"][i]] = 1
 
 one_bf.drop("index", inplace=True, axis=1)
 
 for LGA in livabilities:
-    livabilities[LGA] = livabilities[LGA]/10
-    print(LGA)
-
+    livabilities[LGA] = livabilities[LGA]/counts[LGA]
+print("count:", len(counts))
+print("livabilities: ", len(livabilities))
 livabilities = dict(sorted(livabilities.items(), key=lambda item: item[1]))
 
 print(one_bf)
@@ -29,5 +32,9 @@ for i in range(10):
     print(list(livabilities.items())[i])
     f.write(str(list(livabilities.items())[i]))
     f.write("\n")
+
+print("\n")
+for i in range(1, 11):
+    print(list(livabilities.items())[-i])
      
 f.close() 
